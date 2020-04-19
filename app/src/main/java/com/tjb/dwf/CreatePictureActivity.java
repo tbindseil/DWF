40fa0f279c341a1f7fa73d07e9c8fdb55c9f81fb
TJ <tjbindseil@gmail.com>
2020-04-19 17:49:28 -0600

    send create picture post request wip

--- app/src/main/java/com/tjb/dwf/CreatePictureActivity.java
+++ app/src/main/java/com/tjb/dwf/CreatePictureActivity.java
@@ -6,6 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.w3c.dom.Text;
 
@@ -17,7 +25,29 @@ public class CreatePictureActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_picture);
     }
 
+    // TODO send title and hopefully username!
     public void onClickSubmit(View v) {
-        // make request for new picture
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://localhost:8000";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // do stuff
+                        Toast.makeText(getApplicationContext(),"it worked", Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // do stuff
+                Toast.makeText(getApplicationContext(),"it didn't work", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
