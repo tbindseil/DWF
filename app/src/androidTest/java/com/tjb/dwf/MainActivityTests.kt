918235f300c92d739eef7df572a7a74e6a4d6df6
TJ <tjbindseil@gmail.com>
2020-06-09 21:00:44 -0600

    wtf.... but got launchActivity to compile

--- app/src/androidTest/java/com/tjb/dwf/MainActivityTests.kt
+++ app/src/androidTest/java/com/tjb/dwf/MainActivityTests.kt
@@ -1,32 +1,14 @@
 package com.tjb.dwf
 
-import androidx.lifecycle.Lifecycle
-import androidx.test.espresso.intent.Intents.intended
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import androidx.test.espresso.intent.rule.IntentsTestRule
-import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.core.app.launchActivity
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.LargeTest
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-@LargeTest
 class MainActivityTests {
-    @get:Rule var activityScenarioRule = activityScenarioRule<MainActivity>()
-
-    @get:Rule
-    val intentsTestRule = IntentsTestRule(MainActivity::class.java)
-
-    @Test
-    fun onResume_startsLoginActivity_whenUserDataIsNotFound() {
-        activityScenarioRule.launch()
-        var scenario = activityScenarioRule.scenario
-        scenario.moveToState(Lifecycle.State.CREATED)
-        scenario.moveToState(Lifecycle.State.STARTED)
-        scenario.moveToState(Lifecycle.State.RESUMED)
-
-        intended(hasComponent(LoginActivity::class.java.getName()))
+    @Test fun testEvent() {
+        val scenario = launchActivity<MainActivity>()
     }
 }
+
