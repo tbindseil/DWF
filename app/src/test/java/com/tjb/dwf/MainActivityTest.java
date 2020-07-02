65f5f2e73a1d306c73c4bd73523477d4317ddbec
TJ <tjbindseil@gmail.com>
2020-07-01 19:38:32 -0600

    get activity tests to work on local machine with robolectric

--- app/src/test/java/com/tjb/dwf/MainActivityTest.java
+++ app/src/test/java/com/tjb/dwf/MainActivityTest.java
@@ -2,22 +2,25 @@ package com.tjb.dwf;
 
 import android.app.Instrumentation;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.test.espresso.intent.Intents;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
 import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.Intents.intending;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 
-@RunWith(AndroidJUnit4.class)
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = Build.VERSION_CODES.P)
 public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> mainActivityTestRule =
